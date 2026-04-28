3b7941e7c0729300422561d1bbe3541546c7c840
장주원 <161185680+wndnjs11@users.noreply.github.com>
2026-04-28 02:21:23 +0000

    0428.1

---
+++ study.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main(void){
+    printf("%s %s %s %s\n","국어", "영어", "수학", "과학");
+    printf("======================\n");
+    printf("%4d %4d %4d %4d\n", 50, 5,100,95);
+    printf("%4d %4d %4d %4d\n", 85, 100,80,95);
+    
+    return 0;
+}
---
+++ 프실/0428.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main(void){
+    char alphabets[5] = {"a","p","p","l","e"};
+    for (int i =0; i<alphabets; i++){
+        printf("%c",alphabets[i],1);
+    }
+    return 0;
+} 
