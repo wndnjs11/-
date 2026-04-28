4fe935d6b3e96781ace90e01dde45c2acc06a6bc
장주원 <161185680+wndnjs11@users.noreply.github.com>
2026-04-28 02:33:32 +0000

    asd

--- 프실/0428.c
+++ 프실/0428.c
@@ -1,9 +1,10 @@
 #include<stdio.h>
 
 int main(void){
-    char alphabets[5] = {"a","p","p","l","e"};
-    for (int i =0; i<alphabets; i++){
-        printf("%c",alphabets[i],1);
+    char alphabets[5] = {'a','p','p','l','e'};
+    for (int i =0; i<sizeof(alphabets); i++){
+        printf("%c",alphabets[i]);
     }
+    printf("\n");
     return 0;
-} 
+}  
